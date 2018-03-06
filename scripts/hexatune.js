a2c1610af6a916752894946bd0adc72908e4e0d3
Jaco Meintjes <jakes.meintjes@gmail.com>
2018-03-05 21:01:55 -0800

    Added event subscription.

--- scripts/hexatune.js
+++ scripts/hexatune.js
@@ -1,4 +1,6 @@
 const FFT_SIZE = 2048;
+const NUM_INPUTS = 1;
+const NUM_OUTPUTS = 1;
 
 class Hexatune {
   constructor() {
@@ -13,7 +15,13 @@ class Hexatune {
       navigator.webkitGetUserMedia ||
       navigator.mozGetUserMedia ||
       navigator.msGetUserMedia;
-  }
+
+    this.subscribers = [];
+  };
+
+  subscribe(subscriber) {
+    this.subscribers.push(subscriber);
+  };
 
   init() {
     // create Web Audio Context
@@ -26,11 +34,12 @@ class Hexatune {
 
     // setup gain node
     this.gainNode = this.audioContext.createGain();
-    this.gainNode.gain.value = 0;
 
-    // create a buffer
-    this.frequencyBuffer = new Float32Array(FFT_SIZE);
-  }
+    // setup hexatune node
+    this.process = this.process.bind(this);
+    this.processor = this.audioContext.createScriptProcessor(FFT_SIZE, NUM_INPUTS, NUM_OUTPUTS);
+    this.processor.onaudioprocess = this.process;
+  };
 
   getUserMedia() {
     navigator.getUserMedia(
@@ -39,13 +48,22 @@ class Hexatune {
         this.sendingAudioData = true;
         this.stream = stream;
         this.microphone = this.audioContext.createMediaStreamSource(stream);
-        this.microphone.connect(this.analyser);
-        this.analyser.connect(this.gainNode);
-        this.analyser.connect(this.audioContext.destination);
+        this.microphone.connect(this.gainNode);
+        this.gainNode.connect(this.analyser);
+        this.analyser.connect(this.processor);
+        this.processor.connect(this.audioContext.destination);
       },
       (error) => {
         console.log(error);
       }
     );
-  }
+  };
+
+  process(event) {
+    // do processing here
+    const input = event.inputBuffer.getChannelData(0);
+    this.subscribers.map(subscriber => {
+      subscriber.dispatchEvent(new CustomEvent('hexatune', { detail: { output: input }}));
+    });
+  };
 }
